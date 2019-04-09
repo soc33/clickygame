767cabe8ff1020137ca79a79e56c93f6f7762605
soc33 <soc33@msn.com>
2019-04-09 19:07:26 -0400

    restructuring how app interacts removes counter

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import PictureButton from "./components/PictureButton";
 import Wrapper from "./components/Wrapper";
 import pictures from "./pictures.json";
 import Navbar from "./components/BootstrapComps/navbar";
+// import Container from "./components/BootstrapComps/container";
 import Counter from "./components/Counter/Counter";
 import "./App.css";
 
 class App extends Component {
   state = { pictures };
+
+  switchPictures() {};
+
+  gameOver() {};
+
+
+
   render() {
     return (
       <div>
-      < Navbar />
-      <Counter />
-      <Wrapper>
-        {this.state.pictures.map(picture => (
-          <PictureButton
-            id={picture.id}
-            key={picture.id}
-            name={picture.name}
-            image={picture.image}
-          />
-        ))}
-      </Wrapper>
+        < Navbar />
+        <Counter />
+        <Wrapper>
+            {this.state.pictures.map(picture => (
+              <PictureButton
+                onClick={this.switchPictures}
+                id={picture.id}
+                key={picture.id}
+                name={picture.name}
+                image={picture.image}
+              />
+            ))}
+        </Wrapper>
       </div>
     );
   }
--- src/components/PictureButton/index.js
+++ src/components/PictureButton/index.js
@@ -1,34 +1,10 @@
 import React from "react";
 import "./style.css";
-import pictures from "../../pictures.json";
+// import pictures from "../../pictures.json";
 
-class PictureButton extends React.Component {
-  state = { pictures }
-  switchPictures = pictures => {
-      let ctr = pictures.length;
-      let temp;
-      let index;
-  
-      // While there are elements in the array
-      while (ctr > 0) {
-  // Pick a random index
-          index = Math.floor(Math.random() * ctr);
-  // Decrease ctr by 1
-          ctr--;
-  // And swap the last element with it
-          temp = pictures[ctr];
-          pictures[ctr] = pictures[index];
-          pictures[index] = temp;
-      }
-      return pictures;
-  }
-
-  render() {
-    return <div className="pictureButton">
-     
-      <img className="picture" alt={this.state.pictures.name} src={this.state.pictures.image} onClick={this.switchPictures} />
-    </div>;
-  }
+function PictureButton(props) {
+  return <div className="pictureButton">
+    <img className="picture" alt={props.name} src={props.image}/>
+  </div>;
 }
-
 export default PictureButton;
