1da22aee4120684ce38fd8c46287f0ffce84c39c
soc33 <soc33@msn.com>
2019-04-09 15:44:55 -0400

    changes favicon and title

--- src/components/BootstrapComps/navbar.js
+++ src/components/BootstrapComps/navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-light bg-light display-4"> Clicky Game 
       <a className="navbar-brand" href="https://getbootstrap.com/">
-        <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""/>
+        <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" className="d-inline-block align-top" alt=""/>
           Powered By Bootstrap
       </a>
     </nav>
