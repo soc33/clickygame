5d9bbd395fa550af71d111892a4405e92b8ea3d8
soc33 <soc33@msn.com>
2019-04-09 23:06:25 -0400

    changes styling

--- src/components/BootstrapComps/navbar.js
+++ src/components/BootstrapComps/navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar (props) {
   return (
-    <nav className="navbar navbar-light bg-light display-4"> {props.children} 
+    <nav className="navbar navbar-dark bg-dark text-warning display-4"> {props.children} 
       <div className="scores">
               Score: {props.score} | Highscore: {props.highscore}
           </div>
